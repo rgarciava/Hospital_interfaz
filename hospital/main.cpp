3d8b0a8ee0a0a95f133ddfbf633570396c7475f1
christian460 <pzapanar@unsa.edu.pe>
2025-07-08 00:01:02 -0500

    Coneccion con mysql

--- hospital/main.cpp
+++ hospital/main.cpp
@@ -9,9 +9,10 @@ int main(int argc, char *argv[])
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("localhost");
+    db.setPort(3306);
     db.setDatabaseName("hospital");
     db.setUserName("root");
-    db.setPassword("");
+    db.setPassword("zapanaromero64");
 
     if (!db.open()) {
         QMessageBox::critical(nullptr, "Error", "No se pudo conectar a la base de datos.");
